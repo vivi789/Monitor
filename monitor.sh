5e8d6ad5b1d407a877ce90768f65fea3d1f9fa4f
ViVi <46598633+vivi789@users.noreply.github.com>
2022-10-06 07:58:15 +0700

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -8,9 +8,18 @@ echo "
 ░░░╚═╝░░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═════╝░░░░╚═╝░░░
 "
 echo "OS: $(cat /etc/os-release | grep -w "PRETTY_NAME" | cut -d "=" -f 2)"
-echo "UPTIME: $(uptime | awk -F "," {'print $1'})"
-echo "LOAD: $(uptime | awk -F ":" {'print $5'})"
-echo "RAM: $(free -h | grep -vE "total|Swap" | awk {'print $3"/"$2'})"
-echo "SWAP: $(free -h | grep -vE "total|Mem" | awk {'print $3"/"$2'})"
+#check network
+if ping -c 1 -q google.com >& /dev/null
+then
+        echo "CHECK INTERNET: OK"
+else
+        echo "CHECK INTERNET: FAILED"
+fi
+echo "UPTIME: $(uptime | awk -F "," '{print $1}')"
+echo "LOAD: $(uptime | awk -F ":" '{print $NF}')"
+echo "RAM: $(free -h | grep -vE "total|Swap" | awk '{print $3"/"$2}')"
+echo "SWAP: $(free -h | grep -vE "total|Mem" | awk '{print $3"/"$2}')"
 echo "DISK:"
 df -h | grep -vE '^Filesystem|tmpfs|cdrom|loop' | awk '{ print $3"/"$2" "$5 " " $1 }'
+echo "INODE:"
+df -hi | grep -vE "tmpfs|Filesystem|udev|boot|tmp" | awk {'print $3"/"$2"   "$5"   "$6'}
